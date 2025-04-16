448c25f077b2a9022cd4643ba12d3ba2ac341058
JIM-GLITCH <343306431@qq.com>
2025-04-16 19:27:44 +0800

    Refactor code structure for improved readability and maintainability

---
+++ main.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "vitest"
+import { compileSimilarToPatternToRegExpPattern } from "./main"
+describe("asdawdaw", () => {
+
+    it("visitor", () => {
+        compileSimilarToPatternToRegExpPattern("1+?")
+        console.log(1)
+    })
+})
